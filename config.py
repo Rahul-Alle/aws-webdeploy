ba4e25adb6d88d90d23450029833ff9fd7d2ba58
Rahul Nanda <rahulalle55@gmail.com>
2021-05-01 00:07:37 +0530

    Python Code

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.clv0ft9d4di1.ap-south-1.rds.amazonaws.com"
+customuser = "adminr"
+custompass = "awsdb504"
+customdb = "employee"
+custombucket = "addemployee54"
+customregion = "ap-south-1"
